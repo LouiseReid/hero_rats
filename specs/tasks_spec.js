056e60ca5c6a0593db906836d16da16f47bdee7f
Scott Murray <rai.five@gmail.com>
2017-11-30 15:24:43 +0000

    Task spec added

---
+++ specs/tasks_spec.js
@@ -0,0 +1,30 @@
+var assert = require("assert");
+var Task = require("../task.js")
+
+describe("Task", function(){
+   beforeEach("Setup", function(){
+      task = new Task("Defeat Sagat", 5, 10, "Food", false);
+   });
+
+   it("should have description", function(){
+      assert.strictEqual(task.description, "Defeat Sagat")
+   });
+
+   it("should have difficulty", function(){
+      assert.strictEqual(task.difficultyLevel, 5)
+   });
+
+   it("should have urgencyLevel", function(){
+      assert.strictEqual(task.urgencyLevel, 10);
+   });
+
+   it("should have reward", function(){
+      assert.strictEqual(task.reward, "Food");
+   });
+
+   it("should have complete status", function(){
+      assert.strictEqual(task.complete, false);
+   });
+
+
+})
