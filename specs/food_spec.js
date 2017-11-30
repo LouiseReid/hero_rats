4beadf317e0d72f8ceedd70337e98283c33714c0
Scott Murray <rai.five@gmail.com>
2017-11-30 16:46:15 +0000

    Test

--- specs/food_spec.js
+++ specs/food_spec.js
@@ -3,7 +3,9 @@ var Food = require("../food.js")
 
 describe("Food", function(){
   beforeEach(function(){
-    food = new Food("pizza", 10)
+    food = new Food("pizza", 10);
+    food2 = new Food("pizza2", 10);
+
   });
   it("should have a name", function(){
     assert.strictEqual(food.name, "pizza")
